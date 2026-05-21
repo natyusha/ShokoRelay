5d7040517ef00628975f845e2f016c6070e0e773
natyusha <985941+natyusha@users.noreply.github.com>
2026-05-21 06:39:26 -0400

    Optimize Tree Element Flex Shrinking
    - allows extremely long file paths to correctly overflow in the vfs browser
    - bump plugin to v0.12.2

--- ShokoRelay/ShokoRelayConstants.cs
+++ ShokoRelay/ShokoRelayConstants.cs
@@ -9,7 +9,7 @@ public static class ShokoRelayConstants
     public const string Name = "Shoko Relay";
 
     /// <summary>Current version string.</summary>
-    public const string Version = "0.12.1";
+    public const string Version = "0.12.2";
 
     /// <summary>Internal API version.</summary>
     public const string ApiVersion = "1";
